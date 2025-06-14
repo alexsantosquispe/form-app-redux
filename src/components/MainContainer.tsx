cd6c1937a6d47c069982f31a0d3727c484baef3a
Alex Santos <alexsantosquispe@gmail.com>
2025-06-14 18:06:06 -0400

    schema

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -2,14 +2,14 @@ import { MultiStepForm } from './form/MultiStepForm';
 
 export const MainContainer = () => {
   return (
-    <div className="flex min-h-[78dvh] w-full overflow-hidden rounded-lg bg-white shadow-md">
+    <div className="flex min-h-[78dvh] w-full overflow-hidden rounded-lg bg-white shadow-lg">
       <div className="flex w-full flex-col items-center py-8">
         <h1 className="text-xl font-bold">Multi Step Form</h1>
         <MultiStepForm />
       </div>
       <img
         src="/public/assets/bg-ideas.png"
-        className="w-[32rem] bg-gray-100 object-contain md:flex"
+        className="w-[32rem] bg-gray-100 object-contain px-8 md:flex"
       />
     </div>
   );
